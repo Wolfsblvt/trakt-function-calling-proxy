070abaa62e4e019b522590bfc3bfff4a1d332abb
Wolfsblvt <wolfsblvt@gmail.com>
2025-03-23 22:43:41 +0100

    Enhance Trakt API integration and data processing
    Add pagination and filtering support for watchlist/history methods
    Implement trending media retrieval with rating enrichment
    Refactor API client to support query parameters and typed responses
    Introduce comprehensive type definitions for Trakt data models
    Improve rating mapping efficiency through shared key generation
    
    Enables more scalable data fetching and better type safety across
    the application while expanding available Trakt API functionality

--- src/trakt/client.js
+++ src/trakt/client.js
@@ -3,7 +3,7 @@ import { config } from '../config.js';
 import { parseJson } from '../utils.js';
 
 const TRAKT_API_URL = 'https://api.trakt.tv';
-const TRAKT_API_VERSION = 2;
+const TRAKT_API_VERSION = '2';
 
 class TraktClient {
     /** @type {string} */
@@ -41,6 +41,7 @@ class TraktClient {
     async #makeRequest(endpoint, initOptions = {}, { retry = true } = {}) {
         await this.#ensureTokenValid();
         const url = endpoint;
+        /** @type {import('node-fetch').HeadersInit} */
         const headers = {
             'Authorization': `Bearer ${this.#accessToken}`,
             'trakt-api-version': TRAKT_API_VERSION,
@@ -87,7 +88,7 @@ class TraktClient {
                 throw new Error(`Failed to refresh token: ${response.status}`);
             }
 
-            /** @type {OAuthTokenResponse} */
+            /** @type {import('./trakt-types.js').OAuthTokenResponse} */
             const data = await parseJson(response);
             this.#accessToken = data.access_token;
             this.#refreshToken = data.refresh_token;
@@ -98,21 +99,106 @@ class TraktClient {
         }
     }
 
-
-    async getWatchlist() {
-        return this.#makeRequest(`${TRAKT_API_URL}/users/me/watchlist`);
+    /**
+     * Builds a URL with query parameters
+     * @param {string} baseUrl - The base URL
+     * @param {Object} [params={}] - Query parameters to add
+     * @returns {string} - The complete URL with query parameters
+     */
+    #buildUrl(baseUrl, params = {}) {
+        const url = new URL(baseUrl);
+        Object.entries(params).forEach(([key, value]) => {
+            if (value !== undefined && value !== null) {
+                url.searchParams.append(key, String(value));
+            }
+        });
+        return url.toString();
     }
 
-    /** @returns {Promise<Array>} */
-    async getHistory() {
-        // TODO: Implement API call to fetch history
-        return [];
+    /**
+     * Get the user's watchlist
+     * @param {Object} [options] - Optional parameters
+     * @param {string} [options.type] - Filter by type: movies, shows, seasons, episodes
+     * @param {string} [options.sort='rank'] - How to sort: rank, added, released, title
+     * @param {number} [options.limit] - Number of items to return
+     * @param {number} [options.page=1] - Page number
+     * @returns {Promise<import('./trakt-types.js').WatchlistItem[]>}
+     */
+    async getWatchlist({ type, sort = 'rank', limit, page = 1 } = {}) {
+        const url = this.#buildUrl(`${TRAKT_API_URL}/users/me/watchlist`, { type, sort, limit, page });
+        return this.#makeRequest(url);
     }
 
-    /** @returns {Promise<Array>} */
-    async getRatings() {
-        // TODO: Implement API call to fetch ratings
-        return [];
+    /**
+     * Get the user's watch history
+     * @param {Object} [options] - Optional parameters
+     * @param {string} [options.type] - Filter by type: movies, shows, seasons, episodes
+     * @param {string} [options.id] - Trakt ID for a specific item
+     * @param {string} [options.startAt] - Start date in ISO format (YYYY-MM-DD)
+     * @param {string} [options.endAt] - End date in ISO format (YYYY-MM-DD)
+     * @param {number} [options.limit] - Number of items to return
+     * @param {number} [options.page=1] - Page number
+     * @returns {Promise<import('./trakt-types.js').HistoryItem[]>}
+     */
+    async getHistory({ type, id, startAt, endAt, limit, page = 1 } = {}) {
+        const params = { start_at: startAt, end_at: endAt, limit, page };
+
+        let url;
+        if (type && id) {
+            url = this.#buildUrl(`${TRAKT_API_URL}/users/me/history/${type}/${id}`, params);
+        } else if (type) {
+            url = this.#buildUrl(`${TRAKT_API_URL}/users/me/history/${type}`, params);
+        } else {
+            url = this.#buildUrl(`${TRAKT_API_URL}/users/me/history`, params);
+        }
+
+        return this.#makeRequest(url);
+    }
+
+    /**
+     * Get the user's ratings
+     * @param {Object} [options] - Optional parameters
+     * @param {string} [options.type] - Filter by type: movies, shows, seasons, episodes
+     * @param {string} [options.rating] - Filter by rating (1-10)
+     * @param {number} [options.limit] - Number of items to return
+     * @param {number} [options.page=1] - Page number
+     * @returns {Promise<import('./trakt-types.js').RatingItem[]>}
+     */
+    async getRatings({ type, rating, limit, page = 1 } = {}) {
+        let url;
+        if (type) {
+            url = this.#buildUrl(`${TRAKT_API_URL}/users/me/ratings/${type}`, { rating, limit, page });
+        } else {
+            url = this.#buildUrl(`${TRAKT_API_URL}/users/me/ratings`, { rating, limit, page });
+        }
+        return this.#makeRequest(url);
+    }
+
+    /**
+     * Get trending movies or shows
+     * @param {Object} [options] - Optional parameters
+     * @param {string} [options.type='movies'] - Type: movies or shows
+     * @param {number} [options.limit=10] - Number of items to return
+     * @param {number} [options.page=1] - Page number
+     * @returns {Promise<import('./trakt-types.js').TrendingItem[]>}
+     */
+    async getTrending({ type = 'movies', limit = 10, page = 1 } = {}) {
+        const url = this.#buildUrl(`${TRAKT_API_URL}/trending/${type}`, { limit, page });
+        return this.#makeRequest(url);
+    }
+
+    /**
+     * Search for movies, shows, episodes, people, or lists
+     * @param {Object} options - Search parameters
+     * @param {string} options.query - Search query
+     * @param {string} [options.type] - Filter by type: movie, show, episode, person, list
+     * @param {number} [options.limit=10] - Number of items to return
+     * @param {number} [options.page=1] - Page number
+     * @returns {Promise<import('./trakt-types.js').SearchResult[]>}
+     */
+    async search({ query, type, limit = 10, page = 1 }) {
+        const url = this.#buildUrl(`${TRAKT_API_URL}/search/${type || 'movie,show'}`, { query, limit, page });
+        return this.#makeRequest(url);
     }
 }
 
--- src/trakt/trakt-types.js
+++ src/trakt/trakt-types.js
@@ -4,3 +4,99 @@
  * @property {string} refresh_token
  * @property {number} expires_in
  */
+
+/**
+ * @typedef {Object} TraktIds
+ * @property {number} trakt - Trakt ID
+ * @property {string} [slug] - Slug
+ * @property {string} [imdb] - IMDB ID
+ * @property {number} [tmdb] - TMDB ID
+ * @property {number} [tvdb] - TVDB ID
+ */
+
+/**
+ * @typedef {Object} TraktMovie
+ * @property {string} title - Movie title
+ * @property {number} year - Release year
+ * @property {TraktIds} ids - Movie IDs
+ */
+
+/**
+ * @typedef {Object} TraktShow
+ * @property {string} title - Show title
+ * @property {number} year - First air year
+ * @property {TraktIds} ids - Show IDs
+ */
+
+/**
+ * @typedef {Object} TraktSeason
+ * @property {number} number - Season number
+ * @property {TraktIds} ids - Season IDs
+ */
+
+/**
+ * @typedef {Object} TraktEpisode
+ * @property {number} season - Season number
+ * @property {number} number - Episode number
+ * @property {string} title - Episode title
+ * @property {TraktIds} ids - Episode IDs
+ */
+
+/**
+ * @typedef {Object} TraktPerson
+ * @property {string} name - Person name
+ * @property {TraktIds} ids - Person IDs
+ */
+
+/**
+ * @typedef {Object} WatchlistItem
+ * @property {string} listed_at - ISO 8601 UTC datetime when the item was added to the watchlist
+ * @property {string} type - Type of item: movie, show, season, episode
+ * @property {TraktMovie|TraktShow|TraktSeason|TraktEpisode} [movie] - Movie data if type is movie
+ * @property {TraktMovie|TraktShow|TraktSeason|TraktEpisode} [show] - Show data if type is show
+ * @property {TraktMovie|TraktShow|TraktSeason|TraktEpisode} [season] - Season data if type is season
+ * @property {TraktMovie|TraktShow|TraktSeason|TraktEpisode} [episode] - Episode data if type is episode
+ */
+
+/**
+ * @typedef {Object} HistoryItem
+ * @property {number} id - History ID
+ * @property {string} watched_at - ISO 8601 UTC datetime when the item was watched
+ * @property {string} action - Action (scrobble, checkin, etc.)
+ * @property {string} type - Type of item: movie, show, season, episode
+ * @property {TraktMovie} [movie] - Movie data if type is movie
+ * @property {TraktShow} [show] - Show data if type is show
+ * @property {TraktSeason} [season] - Season data if type is season
+ * @property {TraktEpisode} [episode] - Episode data if type is episode
+ */
+
+/**
+ * @typedef {Object} RatingItem
+ * @property {number} rating - Rating (1-10)
+ * @property {string} rated_at - ISO 8601 UTC datetime when the item was rated
+ * @property {string} type - Type of item: movie, show, season, episode
+ * @property {TraktMovie} [movie] - Movie data if type is movie
+ * @property {TraktShow} [show] - Show data if type is show
+ * @property {TraktSeason} [season] - Season data if type is season
+ * @property {TraktEpisode} [episode] - Episode data if type is episode
+ */
+
+/**
+ * @typedef {Object} TrendingItem
+ * @property {number} watchers - Number of users watching
+ * @property {TraktMovie|TraktShow} movie - Movie data if type is movie
+ * @property {TraktMovie|TraktShow} show - Show data if type is show
+ */
+
+/**
+ * @typedef {Object} SearchResult
+ * @property {string} type - Type of result: movie, show, episode, person, list
+ * @property {number} score - Search score
+ * @property {TraktMovie} [movie] - Movie data if type is movie
+ * @property {TraktShow} [show] - Show data if type is show
+ * @property {TraktEpisode} [episode] - Episode data if type is episode
+ * @property {TraktPerson} [person] - Person data if type is person
+ * @property {Object} [list] - List data if type is list
+ */
+
+export { };
